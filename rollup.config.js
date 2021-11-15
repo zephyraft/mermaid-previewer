3afe667138761c5f52b40f11bfdae7d150a20637
zephyr <zzzzzzzephyr@gmail.com>
2021-11-15 14:10:58 +0800

    [update] remove debugger

--- rollup.config.js
+++ rollup.config.js
@@ -1,54 +1,63 @@
-import {nodeResolve} from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import copy from 'rollup-plugin-copy';
-import del from 'rollup-plugin-delete';
-import postcss from 'rollup-plugin-postcss'
-import strip from '@rollup/plugin-strip';
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import copy from "rollup-plugin-copy";
+import del from "rollup-plugin-delete";
+import postcss from "rollup-plugin-postcss";
+import strip from "@rollup/plugin-strip";
 
 const outputDir = "dist";
 const format = "esm";
+const disableDebug = !(process.env.DEBUG === "true");
+
+let configs = [
+  {
+    input: "src/content/mermaid-render.js",
+    output: {
+      dir: `${outputDir}/src/content`,
+      format: format,
+    },
+    plugins: [
+      // clean dist
+      del({ targets: `${outputDir}/*`, verbose: true }),
+      // resolve dependencies
+      nodeResolve(),
+      commonjs(),
+      // bundle css import
+      postcss(),
+      // copy file
+      copy({
+        targets: [
+          { src: "src/manifest.json", dest: outputDir },
+          { src: "public", dest: outputDir },
+        ],
+      }),
+    ],
+  },
+  {
+    input: "src/option/options.js",
+    output: {
+      dir: `${outputDir}/src/option`,
+      format: format,
+    },
+    plugins: [],
+  },
+  {
+    input: "src/worker/background.js",
+    output: {
+      dir: `${outputDir}/src/worker`,
+      format: format,
+    },
+    plugins: [],
+  },
+];
+
+if (disableDebug) {
+  console.log(`disableDebug=${disableDebug}, add strip plugin.`);
+  configs = configs.map((config) => {
+    config.plugins.push(strip());
+    return config;
+  });
+}
 
 // noinspection JSUnusedGlobalSymbols
-export default [
-    {
-        input: "src/content/mermaid-render.js",
-        output: {
-            dir: `${outputDir}/src/content`,
-            format: format,
-        },
-        plugins: [
-            del({targets: `${outputDir}/*`, verbose: true}),
-            nodeResolve(),
-            commonjs(),
-            postcss(),
-            //
-            strip(),
-            copy({
-                targets: [
-                    {src: "src/manifest.json", dest: outputDir},
-                    {src: "public", dest: outputDir}
-                ]
-            }),
-        ],
-    },
-    {
-        input: "src/option/options.js",
-        output: {
-            dir: `${outputDir}/src/option`,
-            format: format
-        },
-        plugins: [
-            strip(),
-        ],
-    },
-    {
-        input: "src/worker/background.js",
-        output: {
-            dir: `${outputDir}/src/worker`,
-            format: format
-        },
-        plugins: [
-            strip(),
-        ],
-    },
-];
+export default configs;
