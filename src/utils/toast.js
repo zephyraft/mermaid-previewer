52b08a890cd915b98d5c3a2c91970a5e85d11a30
zephyr <zzzzzzzephyr@gmail.com>
2022-07-08 15:53:19 +0800

    [update] typo & toast style

--- src/utils/toast.js
+++ src/utils/toast.js
@@ -8,32 +8,35 @@ import "toastify-js/src/toastify.css";
 import { MESSAGE_TOAST_LEVEL_ERROR } from "./message";
 
 export const toast = async (text, level) => {
+  const commonStyle = {
+    textAlign: "center",
+    borderColor: "rgb(229, 231, 235)",
+    borderWidth: "1px",
+    borderRadius: "1rem",
+  }
+
   const css =
     level === MESSAGE_TOAST_LEVEL_ERROR
       ? {
           style: {
-            color: "rgba(239, 68, 68, 1)",
-            textAlign: "center",
-            borderColor: "rgba(248, 113, 113, 1)",
-            borderWidth: "1px",
-            borderRadius: "0.375rem",
-            background: "rgba(254, 242, 242, 1)",
+            color: "rgb(71, 0, 0)",
+            background: "rgb(248, 114, 114)",
+            ...commonStyle,
           },
         }
       : {
           style: {
-            color: "rgba(59, 130, 246, 1)",
-            textAlign: "center",
-            borderColor: "rgba(96, 165, 250, 1)",
-            borderWidth: "1px",
-            borderRadius: "0.375rem",
-            background: "rgba(239, 246, 255, 1)",
+            color: "rgb(0, 43, 61)",
+            background: "rgb(58, 191, 248)",
+            ...commonStyle,
           },
         };
 
+  console.log(css.style)
+
   Toastify({
     text: text,
-    duration: 3000,
+    duration: 13000,
     style: css.style,
     close: true,
     position: "center",
